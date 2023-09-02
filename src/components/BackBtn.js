8dc04887ebb5b442c8fa03a5105382a9503c1890
Taavish Thaman <taavishthaman97@gmail.com>
2023-09-02 21:08:09 +0530

    fix loading indicator on pressing back button

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -1,25 +1,54 @@
-import { useSelector } from 'react-redux';
-import styles from './BackBtn.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./BackBtn.module.css";
 import { useNavigate } from "react-router-dom";
+import { setQuery, setRegion } from "../features/CountriesList/countriesSlice";
 
 export default function BackBtn() {
-    const navigate = useNavigate()
-    const {isDark} = useSelector(state => state.countries);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isDark } = useSelector((state) => state.countries);
 
-    return <button className={`${styles.btn} ${isDark ? styles.btnDark : ''}`} onClick={() => {
+  return (
+    <button
+      className={`${styles.btn} ${isDark ? styles.btnDark : ""}`}
+      onClick={() => {
+        dispatch(setQuery(""));
+        dispatch(setRegion(""));
         navigate(-1);
-    }}>
-        {
-            isDark ? <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M6.46447 4.10744L7.64298 5.28596L3.75389 9.17504L18.6031 9.17504L18.6031 10.825L3.75389 10.825L7.64298 14.714L6.46447 15.8926L0.57191 10L6.46447 4.10744Z" fill="white"/>
-            </svg> :
-                
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M6.46447 4.10744L7.64298 5.28596L3.75389 9.17504L18.6031 9.17504L18.6031 10.825L3.75389 10.825L7.64298 14.714L6.46447 15.8926L0.57191 10L6.46447 4.10744Z" fill="#111517"/>
-            </svg>
-        }
-        <p>
-            Back
-        </p>
+      }}
+    >
+      {isDark ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M6.46447 4.10744L7.64298 5.28596L3.75389 9.17504L18.6031 9.17504L18.6031 10.825L3.75389 10.825L7.64298 14.714L6.46447 15.8926L0.57191 10L6.46447 4.10744Z"
+            fill="white"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M6.46447 4.10744L7.64298 5.28596L3.75389 9.17504L18.6031 9.17504L18.6031 10.825L3.75389 10.825L7.64298 14.714L6.46447 15.8926L0.57191 10L6.46447 4.10744Z"
+            fill="#111517"
+          />
+        </svg>
+      )}
+      <p>Back</p>
     </button>
+  );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,53 +1,92 @@
-import { useDispatch, useSelector } from 'react-redux';
-import styles from './SearchBar.module.css';
-import { setLoading, setCountries, setError, setQuery, setRegion } from '../features/CountriesList/countriesSlice'
-import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import styles from "./SearchBar.module.css";
+import {
+  setLoading,
+  setCountries,
+  setError,
+  setQuery,
+  setRegion,
+} from "../features/CountriesList/countriesSlice";
+import { useEffect, useRef } from "react";
 
 export default function SearchBar() {
+  const dispatch = useDispatch();
+  const { region, isDark } = useSelector((state) => state.countries);
+  const inputRef = useRef();
 
-    const dispatch = useDispatch();
-    const { region, isDark } = useSelector(state => state.countries);
-    const inputRef = useRef();
-
-    useEffect(() => {
-        if(region) {
-            inputRef.current.value = '';
-        }
-    }, [region])
-
-    async function getCountries(query) {
-        try {
-            dispatch(setLoading());
-            dispatch(setError(''));
-            dispatch(setQuery(query));
-            if(!query) return;
-            const url = `https://restcountries.com/v3.1/name/${query}`;
-            console.log(url)
-            const res = await fetch(url);
-            if(res.status === 404) {
-                dispatch(setCountries([]));
-                return;
-            }
-            const data = await res.json();
-            dispatch(setCountries(data))
-        } catch (e) {
-            dispatch(setError(e.message));
-        } finally {
-            setError('');
-        }   
+  useEffect(() => {
+    if (region) {
+      inputRef.current.value = "";
     }
+  }, [region]);
 
-    async function changeHandler(e) {
-        getCountries(e.target.value);
+  async function getCountries(query) {
+    try {
+      dispatch(setLoading());
+      dispatch(setError(""));
+      dispatch(setQuery(query));
+      if (!query) return;
+      const url = `https://restcountries.com/v3.1/name/${query}`;
+      const res = await fetch(url);
+      if (res.status === 404) {
+        dispatch(setCountries([]));
+        return;
+      }
+      const data = await res.json();
+      dispatch(setCountries(data));
+    } catch (e) {
+      dispatch(setError(e.message));
+    } finally {
+      setError("");
     }
+  }
 
-    return <div className={styles.inputWrapper}>
-        { isDark ? <svg className={styles.searchIcon} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 11H11.7L11.4 10.7C12.4 9.6 13 8.1 13 6.5C13 2.9 10.1 0 6.5 0C2.9 0 0 2.9 0 6.5C0 10.1 2.9 13 6.5 13C8.1 13 9.6 12.4 10.7 11.4L11 11.7V12.5L16 17.5L17.5 16L12.5 11ZM6.5 11C4 11 2 9 2 6.5C2 4 4 2 6.5 2C9 2 11 4 11 6.5C11 9 9 11 6.5 11Z" fill="white"/>
-            </svg> :
-            <svg className={styles.searchIcon} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 11H11.7L11.4 10.7C12.4 9.6 13 8.1 13 6.5C13 2.9 10.1 0 6.5 0C2.9 0 0 2.9 0 6.5C0 10.1 2.9 13 6.5 13C8.1 13 9.6 12.4 10.7 11.4L11 11.7V12.5L16 17.5L17.5 16L12.5 11ZM6.5 11C4 11 2 9 2 6.5C2 4 4 2 6.5 2C9 2 11 4 11 6.5C11 9 9 11 6.5 11Z" fill="#848484"/>
-        </svg>}
-        <input ref={inputRef} type='text' placeholder='Search for a country...' className={`${styles.searchBar} ${isDark ? styles.searchBarDark : ''}`} onChange={(e) => changeHandler(e)}/>
+  async function changeHandler(e) {
+    getCountries(e.target.value);
+  }
+
+  return (
+    <div className={styles.inputWrapper}>
+      {isDark ? (
+        <svg
+          className={styles.searchIcon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12.5 11H11.7L11.4 10.7C12.4 9.6 13 8.1 13 6.5C13 2.9 10.1 0 6.5 0C2.9 0 0 2.9 0 6.5C0 10.1 2.9 13 6.5 13C8.1 13 9.6 12.4 10.7 11.4L11 11.7V12.5L16 17.5L17.5 16L12.5 11ZM6.5 11C4 11 2 9 2 6.5C2 4 4 2 6.5 2C9 2 11 4 11 6.5C11 9 9 11 6.5 11Z"
+            fill="white"
+          />
+        </svg>
+      ) : (
+        <svg
+          className={styles.searchIcon}
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M12.5 11H11.7L11.4 10.7C12.4 9.6 13 8.1 13 6.5C13 2.9 10.1 0 6.5 0C2.9 0 0 2.9 0 6.5C0 10.1 2.9 13 6.5 13C8.1 13 9.6 12.4 10.7 11.4L11 11.7V12.5L16 17.5L17.5 16L12.5 11ZM6.5 11C4 11 2 9 2 6.5C2 4 4 2 6.5 2C9 2 11 4 11 6.5C11 9 9 11 6.5 11Z"
+            fill="#848484"
+          />
+        </svg>
+      )}
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Search for a country..."
+        className={`${styles.searchBar} ${isDark ? styles.searchBarDark : ""}`}
+        onChange={(e) => changeHandler(e)}
+      />
     </div>
+  );
 }
