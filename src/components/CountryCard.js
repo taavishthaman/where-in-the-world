f282a216ab707f30bab7824d58a4dd45342e30e2
Taavish Thaman <taavishthaman97@gmail.com>
2023-09-04 10:36:52 +0530

    change country card background

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,33 +1,81 @@
-import styles from './CountryCard.module.css';
-import { Link } from 'react-router-dom';
-import CountryFlag from './CountryFlag';
-import { useSelector } from 'react-redux';
+import styles from "./CountryCard.module.css";
+import { Link } from "react-router-dom";
+import CountryFlag from "./CountryFlag";
+import { useSelector } from "react-redux";
 
-export default function CountryCard({data}) {
-    const {isDark} = useSelector(state => state.countries);
+export default function CountryCard({ data }) {
+  const { isDark } = useSelector((state) => state.countries);
 
-    return <Link to={`${data.name.common}`} state={{from : data}} className={styles.card}>
-        <div>
-            <div className={styles.countryFlag}>
-                <CountryFlag width={26.4} height={16} src={data.flags.png}/>
-            </div>
-            <div className={`${styles.countryStats} ${isDark ? styles.countryStatsDark : ''}`}>
-                <p className={`${styles.countryName} ${isDark ? styles.countryNameDark : ''}`}>{data.name.common}</p>
-                <div className={styles.stats}>
-                    <div>
-                        <span className={`${styles.statTitle} ${isDark ? styles.statTitleDark : ''}`}>Population: </span> 
-                        <span className={`${styles.statName} ${isDark ? styles.statNameDark : ''}`}>{data.population}</span>
-                    </div>
-                    <div>
-                        <span className={`${styles.statTitle} ${isDark ? styles.statTitleDark : ''}`}>Region: </span> 
-                        <span className={`${styles.statName} ${isDark ? styles.statNameDark : ''}`}>{data.region}</span>
-                    </div>
-                    <div>
-                        <span className={`${styles.statTitle} ${isDark ? styles.statTitleDark : ''}`}>Capital: </span> 
-                        <span className={`${styles.statName} ${isDark ? styles.statNameDark : ''}`}>{data.capital}</span>
-                    </div>
-                </div>
-            </div>
+  return (
+    <Link
+      to={`${data.name.common}`}
+      state={{ from: data }}
+      className={`${styles.card} ${isDark ? styles.cardDark : ""}`}
+    >
+      <div>
+        <div className={styles.countryFlag}>
+          <CountryFlag width={26.4} height={16} src={data.flags.png} />
         </div>
+        <div className={styles.countryStats}>
+          <p
+            className={`${styles.countryName} ${
+              isDark ? styles.countryNameDark : ""
+            }`}
+          >
+            {data.name.common}
+          </p>
+          <div className={styles.stats}>
+            <div>
+              <span
+                className={`${styles.statTitle} ${
+                  isDark ? styles.statTitleDark : ""
+                }`}
+              >
+                Population:{" "}
+              </span>
+              <span
+                className={`${styles.statName} ${
+                  isDark ? styles.statNameDark : ""
+                }`}
+              >
+                {data.population}
+              </span>
+            </div>
+            <div>
+              <span
+                className={`${styles.statTitle} ${
+                  isDark ? styles.statTitleDark : ""
+                }`}
+              >
+                Region:{" "}
+              </span>
+              <span
+                className={`${styles.statName} ${
+                  isDark ? styles.statNameDark : ""
+                }`}
+              >
+                {data.region}
+              </span>
+            </div>
+            <div>
+              <span
+                className={`${styles.statTitle} ${
+                  isDark ? styles.statTitleDark : ""
+                }`}
+              >
+                Capital:{" "}
+              </span>
+              <span
+                className={`${styles.statName} ${
+                  isDark ? styles.statNameDark : ""
+                }`}
+              >
+                {data.capital}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </Link>
+  );
 }
